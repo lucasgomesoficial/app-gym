039c21004c8bf67794cd0487f09b776c24e9c56d
Lucas Gomes <lucasgomesbr@gmail.com>
2024-12-20 13:16:59 -0300

    feat: Validating check-in time

--- src/services/validate-check-in.service.spec.ts
+++ src/services/validate-check-in.service.spec.ts
@@ -1,7 +1,8 @@
-import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { InMemoryCheckInsRepository } from "../repositories/in-memory/in-memory-check-ins.repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found.error";
 import { ValidateCheckInService } from "./validate-check-in.service";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation.error";
 
 let checkInsRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInService;
@@ -10,21 +11,21 @@ describe("Validate Check-in Service", () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInService(checkInsRepository);
-    // vi.useFakeTimers()
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers()
+    vi.useRealTimers();
   });
 
   it("should be able to validate the check-in", async () => {
-    const createdCheckIn = await checkInsRepository.create({
+    const newCheckIn = await checkInsRepository.create({
       gym_id: "gym-01",
       user_id: "user-01",
     });
 
     const { checkIn } = await sut.execute({
-      checkInId: createdCheckIn.id,
+      checkInId: newCheckIn.id,
     });
 
     expect(checkIn.validated_at).toEqual(expect.any(Date));
@@ -37,5 +38,24 @@ describe("Validate Check-in Service", () => {
         checkInId: "inexistent-check-in-id",
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
